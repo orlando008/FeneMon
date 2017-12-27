2a56742fd9846195a656f7918ef4be396a2af7be
klinect <BrianKlinect@gmail.com>
2017-12-27 12:57:57 -0500

    element enum

--- FeneMonLib/Shared/Enumerations.vb
+++ FeneMonLib/Shared/Enumerations.vb
@@ -4,4 +4,22 @@
         LoadGame
         NewGame
     End Enum
+
+    Public Enum ElementEnum
+        Bug
+        Dragon
+        Electric
+        Fighting
+        Fire
+        Flying
+        Ghost
+        Grass
+        Ground
+        Ice
+        Normal
+        Poison
+        Psychic
+        Rock
+        Water
+    End Enum
 End Class
