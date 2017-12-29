bcb2d1a933dda9faadaa709342e98e014e6aaa20
klinect <BrianKlinect@gmail.com>
2017-12-29 08:12:14 -0500

    fixed bug where simulation started with the second move instead of the first

--- FeneMonLib/Methods/SimulatedBattleState.vb
+++ FeneMonLib/Methods/SimulatedBattleState.vb
@@ -7,21 +7,27 @@ Public Class SimulatedBattleState
     Private _defenderCycle As Integer
 
     Public Overrides Function PromptChallengerAction() As BattleAction
-        _challengerCycle += 1
         If _challengerCycle >= Challenger.Moves.Count Then
             _challengerCycle = 0
         End If
 
-        Return SelectRandomAction(Challenger, Defender, _challengerCycle)
+        Dim action As BattleAction = SelectRandomAction(Challenger, Defender, _challengerCycle)
+
+        _challengerCycle += 1
+
+        Return action
     End Function
 
     Public Overrides Function PromptDefenderAction() As BattleAction
-        _defenderCycle += 1
         If _defenderCycle >= Defender.Moves.Count Then
             _defenderCycle = 0
         End If
 
-        Return SelectRandomAction(Defender, Challenger, _defenderCycle)
+        Dim action As BattleAction = SelectRandomAction(Defender, Challenger, _defenderCycle)
+
+        _defenderCycle += 1
+
+        Return action
     End Function
 
     Private Function SelectRandomAction(user As FeneMon, target As FeneMon, cycle As Integer) As BattleAction
--- FeneMonLibTests/BattleTests.vb
+++ FeneMonLibTests/BattleTests.vb
@@ -5,11 +5,22 @@ Imports FeneMonLib
 <TestClass()> Public Class BattleTests
 
     <TestMethod()> Public Sub SimulatedBattleState_ChoosesFirstMove()
-        Dim challenger As New FeneMon(10, 10, 10, 10, 10, 10, {New FeneMonMove("Attack", Enumerations.ElementEnum.Fire, 10, Enumerations.DamageTypeEnum.Physical, 1)})
+        Dim challenger As New FeneMon(10, 10, 10, 10, 10, 10, {New FeneMonMove("Attack", Enumerations.ElementEnum.Fire, 10, Enumerations.DamageTypeEnum.Physical, 1), New FeneMonMove("Attack2", Enumerations.ElementEnum.Fire, 10, Enumerations.DamageTypeEnum.Physical, 1)})
         Dim battle As BattleStateBase = New SimulatedBattleState()
         battle.Challenger = challenger
         Dim action As BattleAction = battle.PromptChallengerAction()
         Assert.AreEqual(challenger.Moves.First, action.Move)
+    End Sub
+
+    <TestMethod()> Public Sub SimulatedBattleState_IdenticalMonsChallengerWins()
+        Dim challenger As New FeneMon(10, 10, 10, 10, 10, 10, {New FeneMonMove("Physical Fire", Enumerations.ElementEnum.Fire, 10, Enumerations.DamageTypeEnum.Physical, 1), New FeneMonMove("Special Ice", Enumerations.ElementEnum.Ice, 10, Enumerations.DamageTypeEnum.Special, 2)})
+        Dim defender As New FeneMon(10, 10, 10, 10, 10, 10, {New FeneMonMove("Physical Fire", Enumerations.ElementEnum.Fire, 10, Enumerations.DamageTypeEnum.Physical, 1), New FeneMonMove("Special Ice", Enumerations.ElementEnum.Ice, 10, Enumerations.DamageTypeEnum.Special, 2)})
+
+        Dim battle As BattleStateBase = New SimulatedBattleState
+        battle.StartBattle(challenger, defender)
+
+        Assert.AreEqual(0, defender.CurrentHealth)
+        Assert.AreNotEqual(0, challenger.CurrentHealth)
     End Sub
 
 End Class
